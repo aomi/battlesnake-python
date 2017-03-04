4b57dc2e278244312789f1c4ea16e9c8e67249b5
vivii-0 <fortier.matt@gmail.com>
2017-03-04 13:24:24 -0800

    Modified node.py to include the weighting function

--- app/node.py
+++ app/node.py
@@ -3,17 +3,17 @@ MAPSIZEY = 20       #Global variable for Y size of map (left-right)
 
 class Node:         #Class for spaces on the board, refered to as Nodes
     def __init__(self, xcoord, ycoord):
-        self.x = xcoord     #X coordinate of this node
-        self.y = ycoord     #Y coordinate of this node
-        self.up = 0         #Reference to node up (0 if wall)
-        self.down = 0       #Reference to node down (0 if wall)
-        self.left = 0       #Reference to node to the left (0 if wall)
-        self.right = 0      #Reference to node to the right (0 if wall)
-        self.content = 0    #Content of node (eg fruit, empty, enemy, etc.)
-        self.weight = 0     #Relative safety of node
-        self.distance = 0   #Distance from current fruit
-        self.netWeight = 0  #Total weighting of node
-        self.parent = 0     #Previous Pathway for Astar
+        self.x = xcoord          #X coordinate of this node
+        self.y = ycoord          #Y coordinate of this node
+        self.up = 0              #Reference to node up (0 if wall)
+        self.down = 0            #Reference to node down (0 if wall)
+        self.left = 0            #Reference to node to the left (0 if wall)
+        self.right = 0           #Reference to node to the right (0 if wall)
+        self.content = "open"    #Content of node (eg food, open, wall (snake bodies are walls), selfHead, otherHead(not implemented))
+        self.weight = 0          #Relative safety of node
+        self.distance = 0        #Distance from current fruit
+        self.netWeight = 0       #Total weighting of node
+        self.parent = 0          #Previous Pathway for Astar
 
 
 class NodeList:
@@ -36,10 +36,6 @@ class NodeList:
     def getList(self):
         return self.data
 
-    def clear(self):
-        for x in range(MAPSIZEX):
-            for y in range(MAPSIZEY):
-                self.data[x][y].content = 0
 
     def connect(self):
         for x in range(MAPSIZEX):
@@ -53,6 +49,28 @@ class NodeList:
                 if self.data[x][y].y < MAPSIZEY-1:         #Checks if node is at the right wall
                     self.data[x][y].right = self.data[x][y+1]
 
+
+    def clear(self):
+        for x in range(MAPSIZEX):
+            for y in range(MAPSIZEY):
+                self.data[x][y].content = "open"
+                self.data[x][y].weight = 0
+
+
+    def 
+
+
+    def weight(self):                                       #Weights every node on the board.
+        for x in range(MAPSIZEX):
+            for y in range(MAPSIZEY):
+                if (self.data[x][y].up == 0) or (self.data[x][y].up.content == "wall"):
+                    self.data[x][y].weight += 2
+                if (self.data[x][y].down == 0) or (self.data[x][y].down.content == "wall"):
+                    self.data[x][y].weight += 2
+                if (self.data[x][y].left == 0) or (self.data[x][y].left.content == "wall"):
+                    self.data[x][y].weight += 2
+                if (self.data[x][y].right == 0) or (self.data[x][y].right.content == "wall"):
+                    self.data[x][y].weight += 2
 
 
 #This function prints the contents of the map as a grid.
